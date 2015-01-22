be042a08b4e9b71691b4a9454f6462a271258e21
efl216 <efl216@lehigh.edu>
2015-01-22 00:33:21 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,32 @@
+// Emily Levenson
+// This is my first program
+// Hello World Java Program
+
+//      First compile the program
+//          javac HelloWorld.java
+//      Then run the program
+//          java HelloWorld// 
+
+
+
+// define a class
+public class HelloWorld{
+    
+    
+// add main method
+    public static void main(String [ ] args) {
+    
+    
+// print the statement with a box around it
+        System.out.println("######################");
+        System.out.println("######################");
+        System.out.println("#                    #");
+        System.out.println("# HELLO WORLD 2014!! #");
+        System.out.println("#                    #");
+        System.out.println("######################");
+        System.out.println("######################");
+        System.out.println("Test");
+        
+        //test/testtest
+    }
+}
