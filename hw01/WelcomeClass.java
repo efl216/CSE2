bd3ab81ca377e4d667119f3c90152588bddc083d
efl216 <efl216@lehigh.edu>
2015-01-23 15:36:48 +0000

    hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+// This is homework 01
+// It will display a Welcome Banner with my network ID
+// Welcome Class java program
+
+
+
+// define a class
+public class WelcomeClass{
+    
+// add main method
+    public static void main(String [ ] args) {
+    
+// print the banner
+        System.out.println("    -----------      ");
+        System.out.println("    | Welcome |      ");
+        System.out.println("    -----------      ");
+        System.out.println("   ^  ^  ^  ^  ^  ^  ");
+        System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println(" <-E--F--L--2--1--6->");
+        System.out.println("  \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("   v  v  v  v  v  v  ");
+        System.out.println(" This past winter break, I skied in Vail, CO with my family. No one got hurt");
+        
+    }
+}
